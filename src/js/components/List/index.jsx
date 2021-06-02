32ca6a9622cf7832c8f2ee262cdaa25f1a6c333c
Kevin Farrugia <hello@imkev.dev>
2021-06-02 18:22:59 +0200

    Implemented solution using react-virtuoso

--- src/js/components/List/index.jsx
+++ src/js/components/List/index.jsx
@@ -1,10 +1,6 @@
 import { nanoid } from "nanoid";
-import React, { useEffect, useRef, useState } from "react";
-import {
-  AutoSizer,
-  List as VirtualList,
-  WindowScroller,
-} from "react-virtualized";
+import React, { useEffect, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
 
 import ListItem from "../ListItem";
 
@@ -13,9 +9,6 @@ const LOREM_IPSUM =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi deserunt, molestiae quidem a.";
 
 const List = () => {
-  const listRef = useRef(null);
-  const heightRef = useRef(0);
-
   const [list, setList] = useState([
     {
       id: nanoid(),
@@ -38,60 +31,16 @@ const List = () => {
     }, INTERVAL);
   }, [list]);
 
-  const rowRenderer = ({
-    key, // Unique key within array of rows
-    index, // Index of row within collection
-    // isScrolling, // The List is currently being scrolled
-    // isVisible, // This row is visible within the List (eg it is not an overscanned row)
-    style, // Style object to be applied to row (to position it)
-  }) => (
-    <ListItem
-      key={key}
-      style={style}
-      img={list[index].img}
-      text={list[index].text}
-    />
-  );
-
   return (
     <section>
       <ul id="list" className="mx-auto max-w-sm p-4">
-        <WindowScroller>
-          {({ height, isScrolling, onChildScroll, scrollTop }) => {
-            let myScrollTop = scrollTop;
-            if (listRef.current && !isScrolling) {
-              // is there a better way to retrieve the clientHeight
-              const newHeight =
-                listRef.current?.Grid._scrollingContainer?.clientHeight;
-
-              // if the user has scrolled down maintain the scroll position
-              if (scrollTop > 0 && heightRef?.current > 0 && newHeight) {
-                myScrollTop = scrollTop + newHeight - heightRef.current;
-              }
-
-              heightRef.current = newHeight;
-            }
-
-            return (
-              <AutoSizer disableHeight>
-                {({ width }) => (
-                  <VirtualList
-                    ref={listRef}
-                    autoHeight
-                    height={height}
-                    onScroll={onChildScroll}
-                    isScrolling={isScrolling}
-                    rowCount={list.length}
-                    rowHeight={308}
-                    rowRenderer={rowRenderer}
-                    scrollTop={myScrollTop}
-                    width={width}
-                  />
-                )}
-              </AutoSizer>
-            );
-          }}
-        </WindowScroller>
+        <Virtuoso
+          style={{ height: "100vh" }}
+          totalCount={list.length}
+          itemContent={(index) => (
+            <ListItem img={list[index].img} text={list[index].text} />
+          )}
+        />
       </ul>
     </section>
   );
